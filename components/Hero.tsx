b44de8cce4636c5f625ffd36224c65c78aec9ee7
Simone <simonedescode@gmail.com>
2024-10-21 23:11:30 +0100

    hero section 1st setup

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">Find the Best Deals, Track the Best Prices</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
