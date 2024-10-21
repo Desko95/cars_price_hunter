41b374a4f1920a3e1a98023256d4f106682567c0
Simone <simonedescode@gmail.com>
2024-10-22 00:31:44 +0100

    1

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,31 @@
+"use client";
 import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
+  const handleScroll = () => {};
+
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title">Find the Best Deals, Track the Best Prices</h1>
+        <h1 className="hero__title">
+          Find the Best Deals, Track the Best Prices
+        </h1>
+        <p className="hero__subtitle">
+          Stay Informed, Stay Ahead – Car Price Tracking Made Easy
+        </p>
+
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay"/>
+        </div>
       </div>
     </div>
   );
